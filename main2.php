e52b9ece28fc3398b380fb5c9ddc3384c6895201
Phanuphong Tanyian <otaroo@hotmail.co.th>
2019-01-19 15:10:08 +0700

    เปลี่ยนรูปแบบหน้าแรก

--- main2.php
+++ main2.php
@@ -4,41 +4,63 @@ require_once "config.php";
 
 <div class="container">
 
-<h1 class="my-4">Welcome to Modern Business</h1>
- <!-- Marketing Icons Section -->
- <div class="row">
+    <h1 class="my-4">ข่าวสาร</h1>
+    <!-- Marketing Icons Section -->
+    <div class="row">
+        <?php
+    $sql ="SELECT * FROM tb_news order by publish_time desc limit 3 ";  
+    $qess=$db->query($sql);	
+    while($fd=$qess->fetch_assoc()){	?>
+
         <div class="col-lg-4 mb-4">
-          <div class="card h-100">
-            <h4 class="card-header">Card Title</h4>
-            <div class="card-body">
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+            <div class="card h-100">
+                <h4 class="card-header">
+                    <?php echo $fd['title'];  ?>
+                </h4>
+                <div class="card-body">
+                    <p class="card-text">
+                        <?php echo $fd['description'];  ?>
+                    </p>
+                    <p class="card-text">วันที่ :
+                        <?php echo $fd['publish_time'];  ?>
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-primary">รายละเอียด</a>
+                </div>
             </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Learn More</a>
-            </div>
-          </div>
         </div>
-        <div class="col-lg-4 mb-4">
-          <div class="card h-100">
-            <h4 class="card-header">Card Title</h4>
-            <div class="card-body">
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis ipsam eos, nam perspiciatis natus commodi similique totam consectetur praesentium molestiae atque exercitationem ut consequuntur, sed eveniet, magni nostrum sint fuga.</p>
+        <?php } ?>
+    </div>
+    <!-- /.row  -->
+
+    <h2>กิจกรรม</h2>
+
+    <div class="row">
+    <?php
+    $sql ="SELECT * FROM tb_activity";  
+    $qess=$db->query($sql);	
+    while($fd=$qess->fetch_assoc()){	?>
+
+        <div class="col-lg-4 col-sm-6 portfolio-item">
+            <div class="card h-100">
+                <a href="#"><img class="card-img-top" src="img/cut10.jpg" alt=""></a>
+                <div class="card-body">
+                    <h4 class="card-title">
+                        <a href="#">  <?php echo $fd['Act_name'];  ?></a>
+                    </h4>
+                    <p class="card-text">วันที่ :
+                        <?php echo $fd['Act_date']; ?></p>
+                </div>
+                <div class="card-footer">
+                    <a href="#" class="btn btn-primary">สมัคร</a>
+                </div>
             </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Learn More</a>
-            </div>
-          </div>
         </div>
-        <div class="col-lg-4 mb-4">
-          <div class="card h-100">
-            <h4 class="card-header">Card Title</h4>
-            <div class="card-body">
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-            </div>
-            <div class="card-footer">
-              <a href="#" class="btn btn-primary">Learn More</a>
-            </div>
-          </div>
-        </div>
-      </div>
+
+        <?php } ?>
+
+    </div>
+    <!-- /.row  -->
+
 </div>
